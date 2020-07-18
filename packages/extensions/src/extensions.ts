00b2c663f9ab4ac6778c490154cb53b964f60b42
Don McCurdy <donmccurdy@google.com>
2020-07-17 21:46:40 -0700

    [extensions] Implement KHR_texture_transform.

--- packages/extensions/src/extensions.ts
+++ packages/extensions/src/extensions.ts
@@ -1,9 +1,11 @@
 import { MaterialsClearcoat } from './khr-materials-clearcoat';
 import { MaterialsUnlit } from './khr-materials-unlit';
 import { MeshQuantization } from './khr-mesh-quantization';
+import { TextureTransform } from './khr-texture-transform';
 
 export * from './khr-materials-clearcoat';
 export * from './khr-materials-unlit';
 export * from './khr-mesh-quantization';
+export * from './khr-texture-transform';
 
-export const KHRONOS_EXTENSIONS = [ MaterialsClearcoat, MaterialsUnlit, MeshQuantization ];
+export const KHRONOS_EXTENSIONS = [ MaterialsClearcoat, MaterialsUnlit, MeshQuantization, TextureTransform ];
